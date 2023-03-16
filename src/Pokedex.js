36192ba9b3068e49ea6d82f765198e11a263a79d
dave-home-2 <davemcelhaney@gmail.com>
2023-03-16 00:13:25 -0400

    fixed flexbox positions

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,13 +2,18 @@ import React from 'react'
 import Pokecard from './Pokecard'
 import './Pokedex.css'
 
-const Pokedex = ({pokemonArray}) => {
+const Pokedex = ({ pokemonArray }) => {
    return (
-      pokemonArray.map(p => 
-      <div className='Pokedex'>
-      <Pokecard id={p.id} name={p.name} type={p.type} bxp={p.base_experience} />
-      </div>
-   ))
+      <>
+         <h1 className='Pokedex-title'>Pokedex</h1>
+         <div className='Pokedex'>
+            {pokemonArray.map(p => (
+               <div className='Pokedex-Pokecards'>
+                  <Pokecard id={p.id} name={p.name} type={p.type} bxp={p.base_experience} />
+               </div>))}
+         </div>
+      </>
+   )
 }
 
 export default Pokedex
